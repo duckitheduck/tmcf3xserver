935d6fff5774e552af06b7959875550ec4d1f793
duckitheduck <ameernas12@gmail.com>
2026-07-12 19:24:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -33,3 +33,83 @@ app.post("/announcement", (req, res) => {
 app.listen(PORT, () => {
     console.log("Listening on " + PORT);
 });
+
+async function update(){
+
+    let servers=await fetch("/servers");
+    servers=await servers.json();
+
+    let logs=await fetch("/logs");
+    logs=await logs.json();
+
+    document.getElementById("serverCount").innerText=servers.length;
+
+    let players=0;
+
+    document.getElementById("servers").innerHTML="";
+
+    servers.forEach(server=>{
+
+        players+=server.players.length;
+
+        let html=`
+
+<div class="server">
+
+<b>JobId</b><br>
+
+${server.jobId}
+
+<br><br>
+
+<b>Players (${server.players.length})</b><br>
+
+`;
+
+        server.players.forEach(player=>{
+
+            html+=`<div class="player">${player}</div>`;
+
+        });
+
+        html+="</div>";
+
+        document.getElementById("servers").innerHTML+=html;
+
+    });
+
+    document.getElementById("playerCount").innerText=players;
+
+    document.getElementById("logCount").innerText=logs.length;
+
+    document.getElementById("logs").innerHTML="";
+
+    logs.forEach(log=>{
+
+        document.getElementById("logs").innerHTML+=`
+
+<div class="log">
+
+<b>${log.type}</b>
+
+(${log.server})
+
+<br>
+
+${log.text}
+
+<br>
+
+<small>${log.time}</small>
+
+</div>
+
+`;
+
+    });
+
+}
+
+update();
+
+setInterval(update,1000);
